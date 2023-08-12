396c15f2465cb3fb556d611158262ee2d4f9f52a
ysi <yshouldi@gmail.com>
2023-08-12 15:48:53 +0530

    changes odne 7

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -8,7 +8,7 @@ import java.util.PriorityQueue;
 public class Main {
     public static void main(String[] args) {
         F1 mercedes = new F1("mercedes", false);
-        mercedes.move(40, 0);
+        mercedes.move(40, 30);
         mercedes.accelerate(20);
         mercedes.accelerate(-60);
       /* List<Integer> li=new ArrayList<>();
